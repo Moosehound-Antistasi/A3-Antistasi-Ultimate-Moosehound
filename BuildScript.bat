6755bc381f6c5d06f1c591bbf36be383de12b380
Kyle J. McKeown <mckeown_k@hotmail.com>
2023-01-13 21:35:30 -0500

    Update `BuildScript.bat`

--- BuildScript.bat
+++ BuildScript.bat
@@ -7,6 +7,12 @@ git clean -fdxq && git reset --hard
 ::RD /S /Q "P:\A3A"
 ::ROBOCOPY ".\A3A" "P:\A3A"
 
-cd .\Tools\ArmaScriptCompiler
-ArmaScriptCompiler.exe
+::cd ".\Tools\ArmaScriptCompiler"
+::ArmaScriptCompiler.exe
+::cd ..\..
+
+cd ".\Tools\Builder"
+powershell -file "buildAddons.ps1"
 cd ..\..
+
+pause
