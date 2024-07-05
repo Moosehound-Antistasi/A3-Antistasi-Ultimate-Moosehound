8b1ce3f6da81075991411bdbf060eac16745d67f
Kyle J. McKeown <mckeown_k@hotmail.com>
2024-07-05 16:45:27 -0400

    Add myself to `enableDebugConsole` array

--- A3A/addons/maps/MissionDescription/debug.hpp
+++ A3A/addons/maps/MissionDescription/debug.hpp
@@ -2,6 +2,7 @@ allowFunctionsLog = 1;
 enableDebugConsole[] = {
     "76561198296934215", // Zets/Westalgie
     "76561198219466935", // SilenceIsFatto
-    "76561198087848420" // Stutpip123
+    "76561198087848420", // Stutpip123
+	"76561197996287940" // Drift_91
 };
 allowFunctionsRecompile = 1;
