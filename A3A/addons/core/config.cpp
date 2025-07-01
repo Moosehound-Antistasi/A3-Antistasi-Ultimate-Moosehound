dcd69de7f967811f173d20d02344cf8c225b8913
Kyle J. McKeown <mckeown_k@hotmail.com>
2025-07-01 01:30:50 -0400

    Attempt to sort Antistasi markers after vanilla

--- A3A/addons/core/config.cpp
+++ A3A/addons/core/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"A3A_Events"};
+        requiredAddons[] = {"A3A_Events", "A3_Ui_F"};
         author = AUTHOR;
         authors[] = { AUTHORS };
         authorUrl = "";
