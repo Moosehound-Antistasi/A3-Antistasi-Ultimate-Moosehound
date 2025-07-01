96067132208c1514dcef5205e28fe219da68f439
Kyle J. McKeown <mckeown_k@hotmail.com>
2025-07-01 04:34:11 -0400

    Revert "Attempt to sort Antistasi markers after vanilla"
    This reverts commit dcd69de7f967811f173d20d02344cf8c225b8913.

--- A3A/addons/core/config.cpp
+++ A3A/addons/core/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"A3A_Events", "A3_Ui_F"};
+        requiredAddons[] = {"A3A_Events"};
         author = AUTHOR;
         authors[] = { AUTHORS };
         authorUrl = "";
