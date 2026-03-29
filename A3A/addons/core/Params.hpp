e8c9310a40f39528e2179e88cf0f66af02dd33be
Kyle J. McKeown <mckeown_k@hotmail.com>
2026-03-29 01:34:11 -0400

    Revert "More fine grained control over garrison group size"
    This reverts commit 6ffdb195eb1d8066532ad000f111af02232a193d.

--- A3A/addons/core/Params.hpp
+++ A3A/addons/core/Params.hpp
@@ -842,8 +842,8 @@ class Params
     {
         title = $STR_params_rebelGarrisonGroupSize;
         tooltip = $STR_params_rebelGarrisonGroupSize_desc;
-        values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-        texts[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"};
+        values[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        texts[] = {"2", "4", "6", "8", "10", "12", "14", "16"};
         default = 8;
     };
     class A3A_UAVSpawnChance: BalanceParams
